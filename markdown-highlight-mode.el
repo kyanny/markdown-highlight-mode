9ecd749819daae6b85308820cb60275b9824197b
Kensuke Nagae <kyanny@gmail.com>
2013-01-16 02:07:21 +0900

    Add important notice about future of this product
    I found the forked version of markdown-mode.el on github and change to use it.
    I have no plan to improve my product so add a notice to .el file.

--- markdown-highlight-mode.el
+++ markdown-highlight-mode.el
@@ -1,3 +1,13 @@
+;; # NOTICE
+;; 
+;; The forked version of markdown-mode is hosted on github https://github.com/milkypostman/markdown-mode
+;; 
+;; It fixes some bugs including the weird behaviour of `dabbrev-expand` (the reason of my motivation).
+;; 
+;; I strongly recommend to use it instead of my poor implementation. I have no plan to improve this product at present.
+;; 
+;; Thank you for your attention.
+
 (setq markdown-highlight-indent-offset 2)
 (setq markdown-highlight-blank-line-re "^ *$")
 
